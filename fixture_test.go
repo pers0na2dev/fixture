65a9e1dd0a3208bdc70489745cd2c63dd054a18e
Alexander <moneyharassment@rambler.ru>
2024-05-07 23:27:23 +0300

    tests: make test table-driven

--- fixture_test.go
+++ fixture_test.go
@@ -4,81 +4,62 @@ import (
 	"testing"
 )
 
-
 type TestStruct struct {
-    ExportedField   int
-    unexportedField int
+	ExportedField   int
+	unexportedField int
 }
 
-func TestWithValidField(t *testing.T) {
-    fixture := NewFixture[TestStruct]()
-    fixture.With("ExportedField", 123)
-
-    if build, _ := fixture.Build(); build.ExportedField != 123 {
-        t.Errorf("Expected ExportedField to be 123, got %v", build.ExportedField)
-    }
-}
-
-func TestWithValidFromConstructor(t *testing.T) {
-    fixture := NewFixture[TestStruct](
-        With{"ExportedField", 123},
-    )
-
-    if build, _ := fixture.Build(); build.ExportedField != 123 {
-        t.Errorf("Expected ExportedField to be 123, got %v", build.ExportedField)
-    }
-}
-
-func TestWithInvalidFieldType(t *testing.T) {
-    fixture := NewFixture[TestStruct]()
-    fixture.With("ExportedField", true)
-
-    _, err := fixture.Build()
-    if err == nil {
-        t.Error("Expected an error, got nil")
-    }
-
-    if err.Error() != "ExportedField: Type mismatch" {
-        t.Errorf("Expected error message to be 'ExportedField: Type mismatch', got %v", err.Error())
-    }
-}
-
-func TestWithInvalidField(t *testing.T) {
-    fixture := NewFixture[TestStruct]()
-    defer func() {
-        if r := recover(); r != nil {
-            t.Errorf("The code panicked with %v", r)
-        }
-    }()
-
-    fixture.With("NonExistentField", 123)
-}
-
-func TestWithUnsettableField(t *testing.T) {
-    fixture := NewFixture[TestStruct]()
-    defer func() {
-        if r := recover(); r != nil {
-            t.Errorf("The code panicked with %v", r)
-        }
-    }()
-
-    fixture.With("unexportedField", 123)
-
-	if build, _ := fixture.Build(); build.unexportedField != 123 {
-		t.Errorf("Expected unexportedField to be 123, got %v", build.unexportedField)
-	}	
-}
-
-func TestBuildWithErrors(t *testing.T) {
-	fixture := NewFixture[TestStruct]()
-	fixture.With("NonExistentField", 123)
-
-	_, err := fixture.Build()
-	if err == nil {
-		t.Error("Expected an error, got nil")
+func TestFixture(t *testing.T) {
+	tests := []struct {
+		name          string
+		field         string
+		value         interface{}
+		expectedError string
+		expectedValue int
+		with          []With
+	}{
+		{
+			name: "WithValidField", field: "ExportedField", value: 123,
+			expectedValue: 123,
+		},
+		{
+			name: "WithInvalidFieldType", field: "ExportedField", value: true,
+			expectedError: "ExportedField: Type mismatch",
+		},
+		{
+			name: "WithInvalidField", field: "NonExistentField", value: 123,
+			expectedError: "NonExistentField: Field is not valid",
+		},
+		{
+			name: "WithUnexportedField", field: "unexportedField", value: 123,
+		},
+		{
+			name: "WithContructor", expectedValue: 456,
+			with: []With{{Name: "ExportedField", Value: 456}},
+		},
 	}
 
-	if err.Error() != "NonExistentField: Field is not valid" {
-		t.Errorf("Expected error message to be 'NonExistentField: Field is not valid', got %v", err.Error())
+	t.Parallel()
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			fixture := NewFixture[TestStruct](tt.with...)
+			fixture.With(tt.field, tt.value)
+			build, err := fixture.Build()
+
+			if tt.expectedError != "" {
+				if err == nil {
+					t.Error("Expected an error, got nil")
+				} else if err.Error() != tt.expectedError {
+					t.Errorf("Expected error message to be '%s', got %v", tt.expectedError, err.Error())
+				}
+			} else {
+				if build.ExportedField != tt.expectedValue {
+					t.Errorf("Expected %s to be %d, got %v", tt.field, tt.expectedValue, build.ExportedField)
+				}
+			}
+		})
 	}
 }
